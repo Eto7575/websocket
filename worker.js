2a5bbf3cc0e79edec0e07ea36a8a0841eca2ef5b
Eto7575 <kaya.ertugrul.hamza@gmail.com>
2026-05-15 09:42:47 +0300

    Add worker.js for handling fetch requests

---
+++ worker.js
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request, env) {
+    const id = env.CONNECTIONS.idFromName('GLOBAL');
+    const stub = env.CONNECTIONS.get(id);
+    return stub.fetch(request);
+  }
+}
